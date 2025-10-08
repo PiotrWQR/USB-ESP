8b6299b268d355403026ce70e5346034a85f0c12
PiotrWQR <6188@akademiazamojska.edu.pl>
2025-10-09 00:00:11 +0200

    save 09.10

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import io
 import serial
 from PyQt5.QtCore import QSize, Qt
@@ -6,10 +7,19 @@ from PyQt5.QtGui import QColor, QPalette
 from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QLabel,
                              QPushButton, QVBoxLayout, QLineEdit, QAction,
                              QMenu, QHBoxLayout, QLayout)
+import json
 from _queue import SimpleQueue
 # dostęp do com musi być zarzadzany asynchronicznie
 #
 import esptool
+request_types = {
+    "set_cca": 4,
+    "get_cca": 2,
+    "set_msg_settings": 5,
+    "get_msg_settings": 3,
+    "none": 0,
+    "get_topology": 1
+}
 
 
 class Color(QWidget):
@@ -28,6 +38,8 @@ def clicked():
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.ser = serial.Serial("COM24", 115200, rtscts=False)
 
         self.setWindowTitle("Ustawienia koordynatora")
         self.layout = QHBoxLayout()
@@ -60,17 +72,19 @@ class MyMainWindow(QMainWindow):
         bemin_label = QLabel("Minimalna ekspotencja odwrotu")
         bemax_label = QLabel("Maksymalna ekspotencja odwrotu")
         retries_label = QLabel("Maksymalna liczba prób")
-        bemin_edit = QLineEdit()
-        bemax_edit = QLineEdit()
-        retries_edit = QLineEdit()
+        self.bemin_edit = QLineEdit()
+        self.bemax_edit = QLineEdit()
+        self.retries_edit = QLineEdit()
         cca_button = QPushButton()
         cca_button.setText("Zatwierź nowe parametry")
+        cca_button.pressed.connect(self.get_cca_data)
+        self.get_cca_data()
         layout.addWidget(bemin_label)
-        layout.addWidget(bemin_edit)
+        layout.addWidget(self.bemin_edit)
         layout.addWidget(bemax_label)
-        layout.addWidget(bemax_edit)
+        layout.addWidget(self.bemax_edit)
         layout.addWidget(retries_label)
-        layout.addWidget(retries_edit)
+        layout.addWidget(self.retries_edit)
         layout.addWidget(cca_button)
 
         self.layout.addLayout(layout)
@@ -96,11 +110,33 @@ class MyMainWindow(QMainWindow):
         layout.addWidget(button)
         self.layout.addLayout(layout)
 
+    def get_cca_data(self):
+        request = {
+            "request_type": 2
+        }
+        self.ser.write(json.dumps(request).encode("utf-8"))
+        json_str = self.ser.readline().strip(b'\n')
+        json_obj = json.loads(json_str)
+        if json_obj['information_type'] == 2:
+            self.bemin_edit.setText(str(json_obj['csma_min_be']))
+            self.bemax_edit.setText(str(json_obj['csma_max_be']))
+            self.retries_edit.setText(str(json_obj['csma_max_backoffs']))
+
+    def set_cca_data(self):
+        # błąd metody
+        print("set_cca_data start")
+        request = dict()
+        request['request_type'] = request_types['set_cca']
+        request['csma_min_be'] = self.bemin_edit.text()
+        request['csma_max_be'] = self.bemax_edit.text()
+        request['csma_max_backoffs'] = self.retries_edit.rext()
+        json_str = json.dumps(request).encode("utf-8")
+        self.ser.write(json_str)
+        self.get_cca_data()
     def attach_topology_window(self):
         layout = QVBoxLayout()
         header_label = QLabel("Topologia")
         neightbours_label = QLabel("Sąsiedzi")
-
 
         self.layout.addLayout(layout)
 
